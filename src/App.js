6c77983bc3781cebfe2a4e9d7a68117d1b6b7490
Joe Downard <joehdownard@gmail.com>
2023-04-02 00:26:14 +0100

    Move stuff around

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import Map, {useControl} from 'react-map-gl';
+import MapboxDraw from '@mapbox/mapbox-gl-draw';
+import { BsBellFill, BsBell } from 'react-icons/bs';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoiam9laGQiLCJhIjoiY2xmeWF5eGluMHB2bDNocXlrODZzZGF3cCJ9.HBhG4s0nCphc0-WQqRwjmA';
+var classNames = require('classnames');
+
 
 function App() {
+
+  const latestParkingReports = [{"location": {"lat": 0, "lng": 0},"spots": 1}, {"location": {"lat": 0, "lng": 0},"spots": 0}]
 
   return (
     <div className="App">
       <h1 className="Title"><span className="Title-first">park</span><span className="Title-second">n</span></h1>
-      <Map/>
+      <ParknMap/>
       <div className="BottomContent">
         <p>About | Join</p>
         <p>Made with <span className="BottomContent-loveHeart">&lt;3</span></p>
@@ -18,26 +24,55 @@ function App() {
   );
 }
 
-function Map() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-2.380683848855683);
-  const [lat, setLat] = useState(51.37721907131301);
-  const [zoom, setZoom] = useState(15);
+function ParknMap(props) {
+  const [notifications, setNotifications] = useState(false);
+  const [drawingSubscriptionArea, setDrawingSubscriptionArea] = useState(false);
 
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom: zoom
-    });
-  });
+//  const mapRef = React.useRef();
+//
+//  const onMapLoad = React.useCallback(() => {
+//    mapRef.current.on('draw.create', () => {
+//      setDrawingSubscriptionArea(false);
+//      setNotifications(true);
+//    });
+//    }, []);
+
+  const handleNotificationsClick = () => {
+    if (drawingSubscriptionArea) {
+      setNotifications(false);
+      return
+    }
+
+    if (notifications) {
+      // Clear notifications
+      setNotifications(false);
+      return
+    }
+
+    setDrawingSubscriptionArea(true);
+  }
 
   return (
           <div className="Map">
-            <div ref={mapContainer} className="map-container" />
+            <div className="Notifications" onClick={handleNotificationsClick}>
+              <div className="Notifications-inner">
+                {
+                  (drawingSubscriptionArea || notifications) ? <BsBellFill className={classNames({
+                    'Notifications-inner--active': notifications,
+                    'Notifications-inner--drawing': drawingSubscriptionArea,
+                  })}/> : <BsBell/>
+                }
+              </div>
+            </div>
+            <Map
+              initialViewState={{
+              longitude: -2.380648016392195,
+                latitude: 51.3772087462643,
+                zoom: 15
+            }}
+              style={{height: '55vh'}}
+              mapStyle="mapbox://styles/mapbox/streets-v9"
+              mapboxAccessToken="pk.eyJ1Ijoiam9laGQiLCJhIjoiY2xmeWltMXQwMDU3ZDNnbXRmdHZwaTJ1biJ9.mQSsSFiAuy7APNzYxa7cWg"/>
           </div>
           );
 }
